a1c3ca8844e3df2fa4008308f472aa1790640cbf
Victor Giraldes <victorpgiraldes@gmail.com>
2022-01-18 18:30:58 -0300

    Add constant to user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ApplicationMailer
   def verify_token
     @user = params[:user]
-    @url  = "http://locahost:3000/users/verify_token?token=#{@user.token}"
+
+    @url  = "#{ENV.fetch('BASE_URL')}/users/verify_token?token=#{@user.token}"
+
     mail(to: @user.email, subject: 'Acesse o email para verificar seu token')
   end
 end
