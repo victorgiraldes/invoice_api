1a44c424183889ff5a3bee82a1d3a5fbefd60848
Victor Giraldes <victorpgiraldes@gmail.com>
2022-01-18 18:29:52 -0300

    Add changes for method current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def authenticate_user
+    head :forbidden unless current_user
+  end
+
+  def current_user
+    user = User.find_by(token: request.headers['Authorization'])
+
+    user.verified? ? user : nil
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     User::RegisterFlow
       .call(email: params[:email])
       .on_success { |result| render json: { user: result[:user] }, status: 200 }
+      .on_failure { |result| render json: { errors: result[:error] }, status: 400 }
   end
 
   def verify_token
