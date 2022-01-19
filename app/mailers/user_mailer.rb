fd238ed8435d77bd4e15e05a6e626ffdeb3cba17
Victor Giraldes <victorpgiraldes@gmail.com>
2022-01-19 17:04:00 -0300

    Update route to verify token

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def verify_token
     @user = params[:user]
 
-    @url  = "#{ENV.fetch('BASE_URL')}/users/verify_token?token=#{@user.token}"
+    @url  = "http://localhost:8080/auth/#{@user.token}"
 
     mail(to: @user.email, subject: 'Acesse o email para verificar seu token')
   end
